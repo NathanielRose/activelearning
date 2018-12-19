394ef9f430a5e0852db4c157cee0834ce0fdcee4
Evan Louie <evan.louie@microsoft.com>
2018-12-18 16:22:28 -0800

    fix windows paths

--- ensemble/management/commands/buildfrontend.py
+++ ensemble/management/commands/buildfrontend.py
@@ -1,23 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
 from subprocess import call
+from os import path
 
 
 class Command(BaseCommand):
     help = "Build the front end for the ensemble visualization (REQUIRED for visualization to work)."
 
     def handle(self, *args, **options):
+        ensemble_path = path.join("ensemble", "static", "ensemble", "foxmovieensemble")
         # Install node_modules
-        call(
-            ["npm", "--prefix", "ensemble/static/ensemble/foxmovieensemble", "install"]
-        )
+        call(["npm", "--prefix", ensemble_path, "install"])
 
         # production build
-        call(
-            [
-                "npm",
-                "run",
-                "--prefix",
-                "ensemble/static/ensemble/foxmovieensemble",
-                "build",
-            ]
-        )
+        call(["npm", "run", "--prefix", ensemble_path, "build"])
